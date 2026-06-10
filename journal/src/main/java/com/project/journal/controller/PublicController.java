713ca3165c19e8538ae885757175c1ff3918bae4
Dhruv Jain <jaindhru005@gmail.com>
2026-06-10 18:08:58 +0530

    added auth to journals service as well

--- journal/src/main/java/com/project/journal/controller/PublicController.java
+++ journal/src/main/java/com/project/journal/controller/PublicController.java
@@ -18,7 +18,7 @@ public class PublicController {
     }
     @PostMapping("/create-user")
     public void createUser(@RequestBody User user){
-        userService.saveEntry(user);
+        userService.saveNewUser(user);
     }
 
 }
--- journal/src/main/java/com/project/journal/controller/UserController.java
+++ journal/src/main/java/com/project/journal/controller/UserController.java
@@ -1,21 +1,14 @@
 package com.project.journal.controller;
 
 import com.project.journal.Repository.UserRepository;
-import com.project.journal.entity.JournalEntry;
 import com.project.journal.entity.User;
-import com.project.journal.service.JournalEntryService;
 import com.project.journal.service.UserService;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -34,7 +27,7 @@ public class UserController {
         User userInDB = userService.findByUserName(userName);
         userInDB.setUserName(user.getUserName());
         userInDB.setPassword(user.getPassword());
-        userService.saveEntry(userInDB);
+        userService.saveNewUser(userInDB);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
